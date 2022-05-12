9d6e9e3ef701c570b15ea1443624c71a2c00eaa7
Maduranga Kodithuwakku <madurangakodithuwakku@gmail.com>
2022-05-12 08:40:42 +0530

    App Changes

--- App.js
+++ App.js
@@ -1,15 +1,84 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
 import Header from "./layouts/Header";
+import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
+import FlashMessage from "react-native-flash-message";
+import GameOverScreen from "./screens/GameOverScreen";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import { showMessage } from "react-native-flash-message";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "poppins-bold": require("./assets/fonts/Poppins-Bold.otf"),
+    "poppins-light": require("./assets/fonts/Poppins-Light.otf"),
+    "poppins-medium": require("./assets/fonts/Poppins-Medium.otf"),
+    "poppins-semiBold": require("./assets/fonts/Poppins-SemiBold.otf"),
+    "poppins-thin": require("./assets/fonts/Poppins-Thin.otf"),
+    "poppins-regular": require("./assets/fonts/Poppins-Regular.otf"),
+  });
+};
 
 export default function App() {
-  return <View style={styles.screen}>
-    <Header title={"Guess A Number"}/>
-  </View>;
+  const [enteredValue, setEnteredValue] = useState();
+  const [playRounded, setPlayRounded] = useState();
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [winningGuess, setWinningGuess] = useState();
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
+
+  if (!isDataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setIsDataLoaded(true)}
+        onError={(error) => {
+          showMessage({
+            message: error,
+            type: "danger",
+          });
+        }}
+      />
+    );
+  }
+
+  const handleSubmit = (number) => {
+    setEnteredValue(number);
+  };
+  const gameOverHandler = (count, winingGuess) => {
+    setPlayRounded(count);
+    setIsGameOver(true);
+    setWinningGuess(winingGuess);
+  };
+  const newGameHandler = () => {
+    setIsGameOver(false);
+    setEnteredValue();
+    setPlayRounded(0);
+    setWinningGuess();
+  };
+  return (
+    <View style={styles.screen}>
+      <Header title={"Guess A Number"} />
+      {enteredValue ? (
+        isGameOver ? (
+          <GameOverScreen
+            playRounded={playRounded}
+            winningGuess={winningGuess}
+            newGameHandler={newGameHandler}
+          />
+        ) : (
+          <GameScreen userChoice={enteredValue} onGameOver={gameOverHandler} />
+        )
+      ) : (
+        <StartGameScreen onSubmitClick={handleSubmit} />
+      )}
+      <FlashMessage position="top" statusBarHeight={50} icon={"auto"} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  screen:{
-    flex:1
-  }
+  screen: {
+    flex: 1,
+  },
 });
