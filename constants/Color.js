3de09e1fc7921a8f09b1c1d0eddc88cac34ce257
Maduranga Kodithuwakku <madurangakodithuwakku@gmail.com>
2022-05-12 08:39:56 +0530

    Colors added

---
+++ constants/Color.js
@@ -0,0 +1,11 @@
+export default {
+  primary: "#4e54c8",
+  backgroundColor: "#FAFBFF",
+  drakNormalTextColor: "#ffff",
+  primaryGradientColorOne: "#4e54c8",
+  primaryGradientColorTwo: "#8f94fb",
+  borderColor: "#828282",
+  surfaceColor: "#ffff",
+  shadowColor: "#000",
+  errorColor: "#EF6666",
+};
