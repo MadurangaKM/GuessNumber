2f2d266860672ffd6665a7a06df34ce03bbe6f69
Maduranga Kodithuwakku <madurangakodithuwakku@gmail.com>
2022-05-12 08:40:10 +0530

    Global Style Added

---
+++ constants/GlobleStyle.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export const GlobalStyle = StyleSheet.create({
+  ButtonText: {
+    fontFamily: "poppins-medium",
+    fontSize: 15,
+  },
+  BodyOne: {
+    fontFamily: "poppins-light",
+    fontSize: 15,
+  },
+  BodyTwo: {
+    fontFamily: "poppins-medium",
+    fontSize: 12,
+  },
+  Heading: {
+    fontFamily: "poppins-bold",
+    fontSize: 20,
+  },
+  Display: {
+    fontFamily: "poppins-medium",
+    fontSize: 50,
+  },
+});
