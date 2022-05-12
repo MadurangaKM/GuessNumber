0835d77c2664a444629363ef6183885f5c4305ab
Maduranga Kodithuwakku <madurangakodithuwakku@gmail.com>
2022-05-12 08:40:35 +0530

    Header Changes

--- layouts/Header.js
+++ layouts/Header.js
@@ -1,28 +1,38 @@
 import React from "react";
-import { StyleSheet, View, Text,Platform } from "react-native";
-import {LinearGradient} from 'expo-linear-gradient';
+import { StyleSheet, Text, Platform } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import Colors from "../constants/Color";
+import { GlobalStyle } from "../constants/GlobleStyle";
 
 const Header = (props) => {
   return (
-    <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#4e54c8', '#8f94fb', '#8f94fb']} style={styles.header}>
-      <Text style={styles.title}>
+    <LinearGradient
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 0 }}
+      colors={[
+        Colors.primaryGradientColorOne,
+        Colors.primaryGradientColorTwo,
+        Colors.primaryGradientColorTwo,
+      ]}
+      style={styles.header}
+    >
+      <Text style={{ ...styles.title, ...GlobalStyle.Heading }}>
         {props.title}
       </Text>
-      </LinearGradient>
+    </LinearGradient>
   );
 };
 const styles = StyleSheet.create({
   header: {
     width: "100%",
-    height: Platform.OS==="ios"?100:80,
-    paddingTop: Platform.OS==="ios"?30:0,
+    height: Platform.OS === "ios" ? 100 : 80,
+    paddingTop: Platform.OS === "ios" ? 30 : 0,
     backgroundColor: "red",
-    justifyContent:"center",
-    alignItems:"center"
+    justifyContent: "center",
+    alignItems: "center",
   },
   title: {
-      color:"#ffff",
-      fontSize:20,
+    color: Colors.drakNormalTextColor,
   },
 });
 export default Header;
