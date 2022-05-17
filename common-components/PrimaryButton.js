fbaa77fb97e703e2ee432acb21c9bfe91d1cc32c
Maduranga Kodithuwakku <madurangakodithuwakku@gmail.com>
2022-05-17 08:48:50 +0530

    icons button added

--- common-components/PrimaryButton.js
+++ common-components/PrimaryButton.js
@@ -1,23 +1,27 @@
 import { StyleSheet, Text, TouchableOpacity, Platform } from "react-native";
 import Colors from "../constants/Color";
 import { GlobalStyle } from "../constants/GlobleStyle";
+import { Ionicons } from '@expo/vector-icons'; 
 
 const Button = (props) => {
   return (
-    <TouchableOpacity
-      onPress={props.onPress}
-      style={{ ...styles.button, ...props.style }}
-    >
-      <Text
-        style={{
-          color: Colors.drakNormalTextColor,
-          paddingTop: Platform.OS === "android" ? 3 : 0,
-          ...GlobalStyle.ButtonText,
-        }}
+      <TouchableOpacity
+        onPress={props.onPress}
+        style={{ ...styles.button, ...props.style }}
       >
-        {props.title}
-      </Text>
-    </TouchableOpacity>
+        {props.isIcon && (
+          <Ionicons name={props.iconName} size={20} color={Colors.drakNormalTextColor} style={{marginRight:4}}/>
+        )}
+        <Text
+          style={{
+            color: Colors.drakNormalTextColor,
+            paddingTop: Platform.OS === "android" ? 3 : 0,
+            ...GlobalStyle.ButtonText,
+          }}
+        >
+          {props.title}
+        </Text>
+      </TouchableOpacity>
   );
 };
 const styles = StyleSheet.create({
@@ -28,6 +32,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     paddingHorizontal: 20,
+    flexDirection: "row",
   },
 });
 
